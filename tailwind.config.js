8decd0914df4693865354cb076ccad0d29964eb6
Abdulqudus Sanusi <qudussanusi2021@gmail.com>
2022-11-09 23:04:00 -0900

    little modifications

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        
+        Softorange: 'hsl(35, 77%, 62%)',
+        Softred: 'hsl(5, 85%, 63%)',
+
+        Offwhite: 'hsl(36, 100%, 99%)',
+        Grayishblue: 'hsl(233, 8%, 79%)',
+        Darkgrayishblue: 'hsl(236, 13%, 42%)',
+        Verydarkblue: 'hsl(240, 100%, 5%)'
       }
     },
   },
